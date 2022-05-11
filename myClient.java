5cfbff645d356e02df3caf4d6d9cea0079ce203d
Victor C <victorchan3@students.mq.edu.au>
2022-05-11 16:02:49 +1000

    Week 8

--- myClient.java
+++ myClient.java
@@ -1,128 +1,87 @@
 import java.io.*;
 import java.net.*;
-import java.util.*;
+import java.util.List;
+import java.util.LinkedList;
 
 public class myClient {
 	public static void main(String[] args) {
 		Socket s = null; //Create a new socket
 		try {
-			// Establish Sockets,
+			//   ./ds-server -c ../../configs/sample-configs/ds-sample-config01.xml -v all -n
+			//Initiation
 			int port = 50000; //Set Server Port == 50000
 			s = new Socket("localhost", port); //Initialise Socket
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream())); //Data will be read from Server with Input Stream
 			// DataInputStream in = new DataInputStream(s.getInputStream());
 			DataOutputStream out = new DataOutputStream(s.getOutputStream()); //Output written to Server with Input Stream
 
-			// Start of HandShake Protocol
-			// Hello [HELO]
-			out.write(("HELO\n").getBytes()); //Client sends "HELO" to Server
-			System.out.println("Sent: HELO");
-			String str = in.readLine(); //Gather Response from Server
-			System.out.println("Received: " + str); //Client receives "HELO" response from Server
 
-			// Authenticate [AUTH]
+			//Initial handshake	
+			//Hello [HELO]		
+			out.write(("HELO\n").getBytes());
+			
+			String str = (String)in.readLine();
+			//Handshake finished
+			
+			//Authenticate [AUTH]
 			String username = System.getProperty("user.name");
-			out.write(("AUTH " + username + "\n").getBytes()); //Client sends "AUTH" to Server to authenticate username
-			System.out.println("Sent: AUTH");
-			str = in.readLine(); //Gather Response from Server
-			System.out.println("Received: " + str); //Client receives "AUTH" response from Server 
+			out.write(("AUTH " + username + "\n").getBytes());
+			
+			str = (String)in.readLine();
 
-			// Ready [REDY]
-			out.write(("REDY\n").getBytes()); //Client sends "REDY" command for next job
-			System.out.println("Sent: REDY");
-			str = in.readLine(); //Gather Response from Server
-			System.out.println("Received: " + str); //Client receives "REDY" response from Server 
-
-			// Get Servers
-			out.write(("GETS All\n").getBytes()); //Client sends "GETS" command to Server
-			System.out.println("Sent: GETS All");
-
-			str = in.readLine();
-			System.out.println("Received: " + str); //Client receives Available Server Amount
-
-			out.write(("OK\n").getBytes());
-			System.out.println("Sent: OK");
-
-			int numOfServers = Integer.parseInt(str.split(" ")[1]);
-			List<Server> serverList = new LinkedList<>();
-
-			for (int i = 0; i < numOfServers; i++) {
-				str = in.readLine();
-				String[] serverDataType = str.split(" ");
-				//Server with its respective Data Type from Server Type to Disk is added to serverList
-				serverList.add(new Server(serverDataType[0], serverDataType[1], serverDataType[2], serverDataType[3], serverDataType[4], serverDataType[5], serverDataType[6], serverDataType[7]));
-			}
-
-			out.write(("OK\n").getBytes());
-			System.out.println("Sent: OK");
-
-			str = in.readLine();
-			System.out.println("Received: " + str);
-
-			// Sort servers in ascending size or Update ServerList
-			serverList.sort(new Comparator<Server>() {
-				public int compare(Server a, Server b) { 
-					return b.coreNo - a.coreNo;
-				}
-			});
-
-			// Determine largest server type
-			String largestServerType = serverList.get(0).Type;
-			List<Server> largestServers = new LinkedList<Server>();
-
-
-			for (Server server : serverList) { //Iterate all servers in severList
-				if (server.Type.equals(largestServerType)) { 
-					largestServers.add(server);
-				} else
-					break;
-			}
-
+			//Ready [REDY]
 			out.write(("REDY\n").getBytes());
-			System.out.println("Sent: REDY");
 
-			str = in.readLine();
-			System.out.println("Received: " + str);
+			str = (String)in.readLine();
 
-			int serverIndex = 0;
+			String job = String.valueOf(str);
 
-			// Assign jobs to the largest servers with Job Scheduler (Largest-Round-Robin)
-			do {
-				String[] ServerDataTypes = str.split(" "); //split the str into an array then save as ServerDataTypes Array
+			// First Capable (FC)
+			while (!job.equals("NONE")) {
+				String[] splStrings = job.split(" ");
 
-				if (ServerDataTypes[0].equals("JOBN") || ServerDataTypes[0].equals("JOBP")) { // Handling of Jobs 
-					Server currentServer = largestServers.get(serverIndex);
-					String JobID = ServerDataTypes[2];
-					String JobSchedule = "SCHD " + JobID + " " + currentServer.Type + " " + currentServer.ID + "\n"; //Sceduling a Job
+				if (splStrings[0].equals("JOBN") || splStrings[0].equals("JOBP")){ // Hand JOBN and JOBP commands
+					//Get Servers
+					out.write(("GETS Capable " + splStrings[4] + " " + splStrings[5] + " " + splStrings[6] + "\n").getBytes());
 
-					out.write(JobSchedule.getBytes());
-					System.out.print("Sent: " + JobSchedule);
+					str = (String)in.readLine();
 
-					str = in.readLine(); //Server sends OK
-					System.out.println("Received: " + str);
+					out.write(("OK\n").getBytes());
 
-					serverIndex++; //Increment Server Index
-					serverIndex = serverIndex % largestServers.size();
+
+					int ServerCount = Integer.parseInt(str.split(" ")[1]); //Get number of servers sent over DATA
+					List<Server> serverList = new LinkedList<>();
+
+					for (int i = 0; i < ServerCount; i++){ //Loop through until all servers have been recorded
+						str = (String)in.readLine(); // Read input, which contains server data
+						String[] splitString = str.split(" "); // Split data
+						serverList.add(new Server(splitString[0], splitString[1], splitString[2], splitString[3], splitString[4], splitString[5], splitString[6], splitString[7]));
+					}
+
+					out.write(("OK\n").getBytes());
+			
+					str = (String)in.readLine();
+					String command = "SCHD " + splStrings[2] + " " + serverList.get(0).Type + " " + serverList.get(0).ID + "\n";
+
+					out.write(command.getBytes()); // Sends SCHD command to current server in the rotation
+
+					str = (String)in.readLine(); // Get the OK command
 				}
 
 				out.write(("REDY\n").getBytes());
-				System.out.println("Sent: REDY");
 
-				str = in.readLine();
-				System.out.println("Received: " + str);
-			} while (!str.equals("NONE"));
+				str = (String)in.readLine();
+				job = String.valueOf(str);
+			}
 
-			// Exit
-			out.write(("QUIT\n").getBytes()); //Client receives "QUIT" command
-			System.out.println("Sent: QUIT");
-
-			str = in.readLine();
-			System.out.println("Received: " + str); //IF "QUIT" is read by client, connection to server is ended
-
-			s.close(); //Ends connection to Server
-		} catch (Exception e) { 
+			// Exit [QUIT]
+			out.write(("QUIT\n").getBytes());
+			
+			str = (String)in.readLine();
+			
+			s.close();
+		} catch (Exception e) {
 			System.out.println(e);
 		}
 	}
-
 }
